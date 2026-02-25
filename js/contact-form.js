88cbd97873ccc5335b6726e2db801d82dbc3f6cb
Netedu Larisa <larisa.netedu@gmail.com>
2026-02-25 14:26:58 +0200

    Lab 1: HTML and CSS reviews

---
+++ js/contact-form.js
@@ -0,0 +1,9 @@
+function submitForm() { 
+    const nume = document.getElementById("nume").value; 
+    const email = document.getElementById("email").value; 
+    const mesaj = document.getElementById("mesaj").value; 
+    console.log("Nume: " + nume); 
+    console.log("Email: " + email); 
+    console.log("Mesaj: " + mesaj); 
+    console.warn("Goodbye Word!");
+}
