41e85b1d80dbc2a71d6b7c7b57703e9f254e82c6
Larisa Netedu <larisa.netedu@gmail.com>
2026-05-13 14:25:54 +0300

    laboratorul 9 + primul exercitiu din laboratorul 10

--- server/index.js
+++ server/index.js
@@ -1,47 +1,62 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Project = require('./models/Project');
+
+mongoose.connect('mongodb://localhost:27017/dashboard')
+ .then(function() {
+ console.log('Conectat la MongoDB!');
+ })
+ .catch(function(err) {
+ console.error('Eroare conectare MongoDB:', err);
+ });
 const app = express();
 const PORT = 3000;
 // Prima ruta: raspunde la GET /
-app.get('/', function(req, res) {
- res.json({ message: 'Serverul functioneaza!' });
+app.get('/api/projects', async function(req, res) {
+ try {
+ const projects = await Project.find();
+ res.json(projects);
+ } catch (err) {
+ res.status(500).json({ error: 'Eroare ' + err });
+ }
 });
+
 // Porneste serverul
 // Date (temporar in memorie, vom folosi MongoDB mai tarziu)
-const projects = [
- { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
- { id: 2, title: "Calculator Buget", tech: "JS", done: true },
- { id: 3, title: "Dashboard React", tech: "React", done: false },
- { id: 4, title: "API Meteo", tech: "React, API", done: false },
-];
+
 
 app.use(express.json());
 
-app.post('/api/projects', function(req, res) {
- const newProject = {
- id: projects.length + 1,
+app.post('/api/projects', async function(req, res) {
+ try {
+ const newProject = new Project({
  title: req.body.title,
  tech: req.body.tech,
  done: req.body.done || false,
- };
- projects.push(newProject);
- res.status(201).json(newProject);
+ });
+ const saved = await newProject.save();
+ res.status(201).json(saved);
+ } catch (err) {
+ res.status(400).json({ error: err.message });
+ }
 });
 
 // GET /api/projects - returneaza toate proiectele
-app.get('/api/projects', function(req, res) {
+/*app.get('/api/projects', function(req, res) {
  res.json(projects);
-});
+});*/
+
 // GET /api/projects/:id
-app.get('/api/projects/:id', function(req, res) {
- const project = projects.find(p => p.id === parseInt(req.params.id));
-  if (!project) {
+app.get('/api/projects/:id', async function(req, res) {
+    const project = await Project.findById(req.params.id);
+    if (!project) {
     return res.status(404).json({ error: 'Not found' });
   }
   res.json(project);
 });
 
 // GET /api/stats
-app.get('/api/stats', function(req, res) {
+/*app.get('/api/stats', function(req, res) {
     const proiecte = projects.length;
     const proiecte_temrinate = projects.filter(p => p.done === true).length;     // done este variabila booleana
     const proiecte_neterminate = projects.filter(p => p.done === false).length;
@@ -51,15 +66,13 @@ app.get('/api/stats', function(req, res) {
         nefinalizate : proiecte_neterminate
     })
     
-});
+});*/
 
- app.delete('/api/projects/:id', function(req, res) {
-    const id_citit = parseInt(req.params.id);
-    const id_index = projects.findIndex(p => p.id === id_citit);
-    if(id_index == -1){
+ app.delete('/api/projects/:id', async function(req, res) {
+    const id_citit = await Project.findByIdAndDelete(req.params.id);
+    if(!id_citit){
         return res.status(404).json({ error: 'Not found' });
     }
-    projects.splice(id_index, 1);
     res.json({ message: 'Deleted' });
 })
 
